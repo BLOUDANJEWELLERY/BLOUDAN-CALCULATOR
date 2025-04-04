0386bbbbe63ed3ddd2bbdae4c694186e1b90c458
BLOUDANJEWELLERY <161647023+BLOUDANJEWELLERY@users.noreply.github.com>
2025-04-04 13:06:14 +0000

    Changes to be committed: 	modified:   index.html 	modified:   manifest.json 	modified:   service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,39 +1,20 @@
-const CACHE_NAME = "bloudan-calculator-cache-v1";
-const FILES_TO_CACHE = [
-  "index.html",
-  "manifest.json",
-  "icon-192.png",
-  "icon-512.png"
-];
-
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(FILES_TO_CACHE);
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open('bloudan-calculator').then(function(cache) {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/manifest.json',
+        '/service-worker.js'
+      ]);
     })
   );
-  self.skipWaiting();
 });
 
-self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(keyList => {
-      return Promise.all(
-        keyList.map(key => {
-          if (key !== CACHE_NAME) {
-            return caches.delete(key);
-          }
-        })
-      );
-    })
-  );
-  self.clients.claim();
-});
-
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
     })
   );
 });
